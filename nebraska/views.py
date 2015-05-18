8c7db021c25314e2eaf937276e296c920f75346b
Jessica Dussault <jdussault4@gmail.com>
2015-05-18 15:02:13 -0500

    Removes unnecessary code from newspapers view

--- nebraska/views.py
+++ nebraska/views.py
@@ -24,45 +24,54 @@ from chronam.core.utils.utils import HTMLCalendar, create_crumbs
 
 # Static pages with no variables, etc
 def places(request):
+  page_title = "Newspapers by City"
   return render_to_response('places.html', 
                             dictionary=locals(), 
                             context_instance=RequestContext(request))
 
 def nebraska_publishing(request):
+  page_title = "Publishing History"
   return render_to_response('nebraska_publishing.html',
                             dictionary=locals(),
                             context_instance=RequestContext(request))
 
 def adding(request):
+  page_title = "Adding Newspapers"
   return render_to_response('adding.html',
                             dictionary=locals(),
                             context_instance=RequestContext(request))
 
 def nnp(request):
+  page_title = "The Nebraska Newspaper Project"
   return render_to_response('nnp.html',
                             dictionary=locals(),
                             context_instance=RequestContext(request))
 def ndnp(request):
+  page_title = "The National Digital Newspaper Project"
   return render_to_response('ndnp.html',
                             dictionary=locals(),
                             context_instance=RequestContext(request))
 
 def access(request):
+  page_title = "Access: Finding Nebraska Newspapers"
   return render_to_response('access.html',
                             dictionary=locals(),
                             context_instance=RequestContext(request))
 
 def contact(request):
+  page_title = "Contact the Nebrasks Newspapers Project"
   return render_to_response('contact.html',
                             dictionary=locals(),
                             context_instance=RequestContext(request))
 
 def fourohfour(request):
+    page_title = "Page Not Found"
     return render_to_response('404.html',
                               dictionary=locals(),
                               context_instance=RequestContext(request))
 
 def fivehundred(request):
+    page_title = "Something is Wrong"
     return render_to_response('500.html',
                               dictionary=locals(),
                               context_instance=RequestContext(request))
@@ -73,105 +82,26 @@ def fivehundred(request):
 # but the url has been left so those queries will
 # hit the newspapers version in the core
 @cache_page(settings.DEFAULT_TTL_SECONDS)
-def newspapers(request, state=None, format='html'):
-    if state and state != "all_states":
-        state = unpack_url_path(state)
-        if state is None:
-            raise Http404
-        else:
-            state = state.title()
-    else:
-        state = request.REQUEST.get('state', None)
-    
-    language = request.REQUEST.get('language', None)
-    if language:
-        language_display = models.Language.objects.get(code__contains=language).name
-    ethnicity = request.REQUEST.get('ethnicity', None)
-
-    if not state and not language and not ethnicity:
-        page_title = 'All Digitized Newspapers'
-    else:
-        page_title = 'Results: Digitized Newspapers'
+def newspapers(request):
+    page_title = 'All Digitized Newspapers'
 
     titles = models.Title.objects.filter(has_issues=True)
     titles = titles.annotate(first=Min('issues__date_issued'))
     titles = titles.annotate(last=Max('issues__date_issued'))
 
-    if state:
-        titles = titles.filter(places__state__iexact=state)
-
-    if language:
-        titles = titles.filter(languages__code__contains=language)
-
-    if ethnicity:
-        try:
-            e = models.Ethnicity.objects.get(name=ethnicity)
-            ethnicity_filter = Q(subjects__heading__icontains=ethnicity)
-            for s in e.synonyms.all():
-                ethnicity_filter |= Q(subjects__heading__icontains=s.synonym)
-            titles = titles.filter(ethnicity_filter)
-        except models.Ethnicity.DoesNotExist:
-            pass
-
     _newspapers_by_state = {}
     for title in titles:
-        if state:
-            _newspapers_by_state.setdefault(state, set()).add(title)
-        else:
-            for place in title.places.all():
-                if place.state:
-                    _newspapers_by_state.setdefault(place.state, set()).add(title)
+        for place in title.places.all():
+            if place.state:
+                _newspapers_by_state.setdefault(place.state, set()).add(title)
 
     newspapers_by_state = [(s, sorted(t, key=lambda title: title.name_normal)) for s, t in sorted(_newspapers_by_state.iteritems())]
     crumbs = list(settings.BASE_CRUMBS)
 
-    if format == "html":
-        return render_to_response("newspapers.html",
-                                  dictionary=locals(),
-                                  context_instance=RequestContext(request))
-    elif format == "txt":
-        host = request.get_host()
-        return render_to_response("newspapers.txt",
-                                  dictionary=locals(),
-                                  context_instance=RequestContext(request),
-                                  mimetype="text/plain")
-    elif format == "csv":
-        csv_header_labels = ('Persistent Link', 'State', 'Title', 'LCCN', 'OCLC', 
-                             'ISSN', 'No. of Issues', 'First Issue Date', 
-                             'Last Issue Date', 'More Info')
-        response = HttpResponse(content_type='text/csv')
-        response['Content-Disposition'] = 'attachment; filename="chronam_newspapers.csv"'
-        writer = csv.writer(response)
-        writer.writerow(csv_header_labels)
-        for state, titles in newspapers_by_state:
-            for title in titles:
-                writer.writerow(('http://%s%s' % (request.get_host(), 
-                                                  reverse('chronam_issues', 
-                                                           kwargs={'lccn': title.lccn}),),
-                                 state, title, title.lccn or '', title.oclc or '',
-                                 title.issn or '', title.issues.count(), title.first, 
-                                 title.last, 
-                                 'http://%s%s' % (request.get_host(),
-                                                  reverse('chronam_title_essays',
-                                                           kwargs={'lccn': title.lccn}),),))
-        return response
-
-    elif format == "json":
-        host = request.get_host()
-        results = {"newspapers": []}
-        for state, titles in newspapers_by_state:
-            for title in titles:
-                results["newspapers"].append({
-                    "lccn": title.lccn,
-                    "title": title.display_name,
-                    "url": "http://" + host + title.json_url,
-                    "state": state
-                })
-
-        return HttpResponse(json.dumps(results, indent=2), mimetype='application/json')
-    else:
-        return HttpResponseServerError("unsupported format: %s" % format)
-
+    return render_to_response("newspapers.html",
+                            dictionary=locals(),
+                            context_instance=RequestContext(request))
+    
 
 @cache_page(settings.DEFAULT_TTL_SECONDS)
 def calendar_issues(request, year=None):
